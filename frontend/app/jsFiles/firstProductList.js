f05aa1cb18b9b9b886fd2a02dea8c213dd8635c8
vishnupvnhrt <vishnu@nhrtechnologies.com>
2025-02-18 21:27:46 +0530

    removing midas

--- frontend/app/jsFiles/firstProductList.js
+++ frontend/app/jsFiles/firstProductList.js
@@ -1,101 +1,106 @@
 let firstProductList = [
-    {
-      name: "Precured Tread Rubber",
-      imageUrl: "https://5.imimg.com/data5/OX/KT/TR/SELLER-5475264/precured-tread-rubber-500x500-1000x1000.jpg"
-    },
-    {
-      name: "Conventional (Hot) Rubber",
-      imageUrl: "https://cpimg.tistatic.com/07815989/b/5/Conventional-Rubber.jpg"
-    },
-    {
-      name: "Bonding Gum",
-      imageUrl: "https://5.imimg.com/data5/SELLER/Default/2020/11/UL/QU/JH/24683302/bonding-gum.jpg"
-    },
-    {
-      name: "Curing Bag",
-      imageUrl: "https://5.imimg.com/data5/CJ/MH/MY-1718646/curing-bag-500x500.jpg"
-    },
-    {
-      name: "Envelope",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1588262546.jpg" // You can choose a primary image for the Envelope category
-    },
-    {
-      name: "Tyre Flap",
-      imageUrl: "https://cpimg.tistatic.com/05384010/b/4/Tyre-Flap.jpg"
-    },
-    {
-      name: "Orbi Tread",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1538127382.jpg"
-    },
-    {
-      name: "Black Compounds",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1581400028.jpg"
-    },
-    {
-      name: "Rope Rubber",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1538127546.jpg"
-    },
-    {
-      name: "Vulcanizing Solution",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1581400262.jpg"
-    },
-    {
-      name: "Round Tread",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1675486748.jpg"
-    },
-    {
-      name: "Cushion Repair Compound",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1581401480.jpg"
-    },
-    {
-      name: "Precured Tread Rubber",
-      imageUrl: "https://5.imimg.com/data5/OX/KT/TR/SELLER-5475264/precured-tread-rubber-500x500-1000x1000.jpg"
-    },
-    {
-      name: "Conventional (Hot) Rubber",
-      imageUrl: "https://cpimg.tistatic.com/07815989/b/5/Conventional-Rubber.jpg"
-    },
-    {
-      name: "Bonding Gum",
-      imageUrl: "https://5.imimg.com/data5/SELLER/Default/2020/11/UL/QU/JH/24683302/bonding-gum.jpg"
-    },
-    {
-      name: "Curing Bag",
-      imageUrl: "https://5.imimg.com/data5/CJ/MH/MY-1718646/curing-bag-500x500.jpg"
-    },
-    {
-      name: "Envelope",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1588262546.jpg" // You can choose a primary image for the Envelope category
-    },
-    {
-      name: "Tyre Flap",
-      imageUrl: "https://cpimg.tistatic.com/05384010/b/4/Tyre-Flap.jpg"
-    },
-    {
-      name: "Orbi Tread",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1538127382.jpg"
-    },
-    {
-      name: "Black Compounds",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1581400028.jpg"
-    },
-    {
-      name: "Rope Rubber",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1538127546.jpg"
-    },
-    {
-      name: "Vulcanizing Solution",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1581400262.jpg"
-    },
-    {
-      name: "Round Tread",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1675486748.jpg"
-    },
-    {
-      name: "Cushion Repair Compound",
-      imageUrl: "https://midas.ind.in//public/uploads/products/1581401480.jpg"
-    }
-  ];
-  
-  export default firstProductList;
-  
+  {
+    name: "Precured Tread Rubber",
+    imageUrl:
+      "https://5.imimg.com/data5/OX/KT/TR/SELLER-5475264/precured-tread-rubber-500x500-1000x1000.jpg",
+  },
+  {
+    name: "Conventional (Hot) Rubber",
+    imageUrl: "https://cpimg.tistatic.com/07815989/b/5/Conventional-Rubber.jpg",
+  },
+  {
+    name: "Bonding Gum",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2020/11/UL/QU/JH/24683302/bonding-gum.jpg",
+  },
+  {
+    name: "Curing Bag",
+    imageUrl:
+      "https://5.imimg.com/data5/CJ/MH/MY-1718646/curing-bag-500x500.jpg",
+  },
+  {
+    name: "Envelope",
+    imageUrl: "https://midas.ind.in//public/uploads/products/1588262546.jpg", // You can choose a primary image for the Envelope category
+  },
+  {
+    name: "Tyre Flap",
+    imageUrl: "https://cpimg.tistatic.com/05384010/b/4/Tyre-Flap.jpg",
+  },
+  {
+    name: "Orbi Tread",
+    imageUrl: "https://midas.ind.in//public/uploads/products/1538127382.jpg",
+  },
+  {
+    name: "Black Compounds",
+    imageUrl: "https://midas.ind.in//public/uploads/products/1581400028.jpg",
+  },
+  {
+    name: "Rope Rubber",
+    imageUrl: "https://midas.ind.in//public/uploads/products/1538127546.jpg",
+  },
+  {
+    name: "Vulcanizing Solution",
+    imageUrl: "/cbond.jpg",
+  },
+  {
+    name: "Round Tread",
+    imageUrl: "https://midas.ind.in//public/uploads/products/1675486748.jpg",
+  },
+  // {
+  //   name: "Cushion Repair Compound",
+  //   imageUrl: "https://midas.ind.in//public/uploads/products/1581401480.jpg"
+  // },
+  {
+    name: "Precured Tread Rubber",
+    imageUrl:
+      "https://5.imimg.com/data5/OX/KT/TR/SELLER-5475264/precured-tread-rubber-500x500-1000x1000.jpg",
+  },
+  {
+    name: "Conventional (Hot) Rubber",
+    imageUrl: "https://cpimg.tistatic.com/07815989/b/5/Conventional-Rubber.jpg",
+  },
+  {
+    name: "Bonding Gum",
+    imageUrl:
+      "https://5.imimg.com/data5/SELLER/Default/2020/11/UL/QU/JH/24683302/bonding-gum.jpg",
+  },
+  {
+    name: "Curing Bag",
+    imageUrl:
+      "https://5.imimg.com/data5/CJ/MH/MY-1718646/curing-bag-500x500.jpg",
+  },
+  {
+    name: "Envelope",
+    imageUrl: "https://midas.ind.in//public/uploads/products/1588262546.jpg", // You can choose a primary image for the Envelope category
+  },
+  {
+    name: "Tyre Flap",
+    imageUrl: "https://cpimg.tistatic.com/05384010/b/4/Tyre-Flap.jpg",
+  },
+  {
+    name: "Orbi Tread",
+    imageUrl: "https://midas.ind.in//public/uploads/products/1538127382.jpg",
+  },
+  {
+    name: "Black Compounds",
+    imageUrl: "https://midas.ind.in//public/uploads/products/1581400028.jpg",
+  },
+  {
+    name: "Rope Rubber",
+    imageUrl: "https://midas.ind.in//public/uploads/products/1538127546.jpg",
+  },
+  {
+    name: "Vulcanizing Solution",
+    imageUrl: "/cbond.jpg",
+  },
+  {
+    name: "Round Tread",
+    imageUrl: "https://midas.ind.in//public/uploads/products/1675486748.jpg",
+  },
+  // {
+  //   name: "Cushion Repair Compound",
+  //   imageUrl: "https://midas.ind.in//public/uploads/products/1581401480.jpg"
+  // }
+];
+
+export default firstProductList;
