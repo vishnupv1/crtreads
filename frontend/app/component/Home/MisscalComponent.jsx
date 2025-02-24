2d795f4b4806fc4904c0d4f241fb5df326390af9
vishnupvnhrt <vishnu@nhrtechnologies.com>
2025-02-25 00:43:22 +0530

    mobile

--- frontend/app/component/Home/MisscalComponent.jsx
+++ frontend/app/component/Home/MisscalComponent.jsx
@@ -6,7 +6,7 @@ function MisscalComponent() {
   return (
     <div className="md:hidden">
       <a
-        href="https://wa.me/919846060006"
+        href="https://wa.me/917736031389"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-4 right-4 w-16 h-16 p-0 bg-green-600 rounded-full shadow-lg overflow-hidden flex items-center justify-center"
