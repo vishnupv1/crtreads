33a175d3379758e6de348c1412fdb2fc8e658736
vishnupvnhrt <vishnu@nhrtechnologies.com>
2025-02-24 23:53:22 +0530

    video compressed

--- frontend/app/component/Home/CarouselComponent.js
+++ frontend/app/component/Home/CarouselComponent.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 import Player from "@vimeo/player";
 const BannerSlider = () => {
   const iframeRef1 = useRef(null);
@@ -23,7 +23,7 @@ const BannerSlider = () => {
 
   useEffect(() => {
     // Dynamically load the Vimeo player script
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = "https://player.vimeo.com/api/player.js";
     script.async = true;
     document.body.appendChild(script);
@@ -35,10 +35,7 @@ const BannerSlider = () => {
   }, []);
 
   return (
-    <section
-      aria-label="Banner"
-      className="relative shadow-md w-full"
-    >
+    <section aria-label="Banner" className="relative shadow-md w-full">
       <article className="h-full w-full">
         <figure className="h-full w-full overflow-hidden">
           {/* Overlay */}
@@ -46,26 +43,38 @@ const BannerSlider = () => {
 
           {/* Vimeo Embed */}
           <div className="h-[500px] overflow-hidden md:hidden">
-            <div style={{ padding: '180.27% 0 0 0', position: 'relative' }}>
+            <div style={{ padding: "180.27% 0 0 0", position: "relative" }}>
               <iframe
-               ref={iframeRef1}
-                src="https://player.vimeo.com/video/1051170649?autoplay=1&loop=1&muted=1&controls=0"
+                ref={iframeRef1}
+                src="https://player.vimeo.com/video/1059820061?autoplay=1&loop=1&muted=1&controls=0"
                 frameBorder="0"
                 allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
-                style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                }}
                 title="crc treads"
               ></iframe>
             </div>
           </div>
 
           <div className="h-[600px] lg:h-[1000px] overflow-hidden hidden md:block">
-            <div style={{ padding: '86.27% 0 0 0', position: 'relative' }}>
+            <div style={{ padding: "86.27% 0 0 0", position: "relative" }}>
               <iframe
-            ref={iframeRef2}
-                src="https://player.vimeo.com/video/1051568333?h=b7dea7afc6&autoplay=1&loop=1&muted=1&controls=0&portrait=0"
+                ref={iframeRef2}
+                src="https://player.vimeo.com/video/1059820061?h=b7dea7afc6&autoplay=1&loop=1&muted=1&controls=0&portrait=0"
                 frameBorder="0"
                 allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
-                style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                }}
                 title="crc treads"
               ></iframe>
             </div>
