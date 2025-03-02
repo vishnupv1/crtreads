208aa8d8be89b027510b8f14cac622da66e1d4c5
vishnupv1 <vishnupv0808@gmail.com>
2025-03-02 20:02:48 +0530

    best sellers

--- frontend/app/jsFiles/BestSeller.js
+++ frontend/app/jsFiles/BestSeller.js
@@ -7,29 +7,57 @@ let bestSeller = [
   //   name: "AJAX",
   //   imageUrl: "https://midas.ind.in//public/uploads/products/1587540827.jpg",
   // },
+  // {
+  //   name: "CRE",
+  //   imageUrl: "/patterns/CRE.jpg",
+  // },
+  // {
+  //   name: "CML",
+  //   imageUrl: "/patterns/cml.jpg",
+  // },
+  // {
+  //   name: "CR580",
+  //   imageUrl: "/patterns/cr580.jpg",
+  // },
+  // {
+  //   name: "CR582",
+  //   imageUrl: "/patterns/cr582.jpg",
+  // },
+  // {
+  //   name: "CRB",
+  //   imageUrl: "/patterns/crb.jpg",
+  // },
+  // {
+  //   name: "CRM",
+  //   imageUrl: "/patterns/crm.jpg",
+  // },
   {
-    name: "CRE",
-    imageUrl: "/patterns/CRE.jpg",
+    name: "CM 02",
+    imageUrl: "CM02.jpg",
   },
   {
-    name: "CML",
-    imageUrl: "/patterns/cml.jpg",
+    name: "CD 01",
+    imageUrl: "CD01.jpg",
   },
   {
-    name: "CR580",
-    imageUrl: "/patterns/cr580.jpg",
+    name: "CD 02",
+    imageUrl: "CD02.jpg",
   },
   {
-    name: "CR582",
-    imageUrl: "/patterns/cr582.jpg",
+    name: "CD 03",
+    imageUrl: "CD03.jpg",
   },
   {
-    name: "CRB",
-    imageUrl: "/patterns/crb.jpg",
+    name: "CE 582",
+    imageUrl: "CE582.jpg",
   },
   {
-    name: "CRM",
-    imageUrl: "/patterns/crm.jpg",
+    name: "CD 04",
+    imageUrl: "CD04.jpg",
+  },
+  {
+    name: "CM 01",
+    imageUrl: "CM01.jpg",
   },
 ];
 
